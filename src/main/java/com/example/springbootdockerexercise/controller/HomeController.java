2c03767d2a4328e457d91b3deb4793e5448bc4df
twg0 <fruitsjh@gmail.com>
2023-05-16 14:50:33 +0900

    feat: add home

--- src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
+++ src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.springbootdockerexercise.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
 
     @ResponseBody
