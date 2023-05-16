9079ffcae95187c2a9a8dea02c306d104927aa96
twg0 <fruitsjh@gmail.com>
2023-05-16 15:09:19 +0900

    feat: add RequestMapping

--- src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
+++ src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
@@ -2,13 +2,15 @@ package com.example.springbootdockerexercise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @ResponseBody
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "Hello";
     }
