2e605f44061777a7936c753ebc6001316d09b212
twg0 <fruitsjh@gmail.com>
2023-05-16 15:47:06 +0900

    feat: dockerfile 및 webhook 수정 - webhook 실패

--- src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
+++ src/main/java/com/example/springbootdockerexercise/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.example.springbootdockerexercise.controller;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
@@ -12,6 +17,25 @@ public class HomeController {
     @GetMapping
     @ResponseBody
     public String home() {
-        return "Hello, World!";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String webhookUrl = "https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU"; // 웹훅 수신 URL
+
+        Date date = new Date();
+        // 웹훅으로 전송할 데이터
+        String payload = "{\"message\": \" ?? \"}";
+
+        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(webhookUrl, HttpMethod.POST, entity, String.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return "Webhook sent successfully";
+        } else {
+            return "Failed to send webhook";
+        }
     }
 }
